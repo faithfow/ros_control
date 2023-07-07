a4531c6985ec9a62c11db631b3ec6de5b0188070
faithfow <128486321+faithfow@users.noreply.github.com>
2023-07-08 01:23:49 +0800

    Add files via upload

---
+++ api.cpp
@@ -0,0 +1,116 @@
+/**
+* \brief 根据话题生成发布对象
+*
+* 在 ROS master 注册并返回一个发布者对象，该对象可以发布消息
+*
+* 
+*
+* \param topic 发布消息使用的话题
+*
+* \param queue_size 等待发送给订阅者的最大消息数量
+*
+* \param latch (optional) 如果为 true,该话题发布的最后一条消息将被保存，并且后期当有订阅者连接时会将该消息发送给订阅者
+*
+* \return 调用成功时，会返回一个发布对象
+*
+*
+*/
+template <class M>
+Publisher advertise(const std::string& topic, uint32_t queue_size, bool latch = false)
+/**
+   * \brief 生成某个话题的订阅对象
+   *
+   * 该函数将根据给定的话题在ROS master 注册，并自动连接相同主题的发布方，每接收到一条消息，都会调用回调
+   * 函数，并且传入该消息的共享指针，该消息不能被修改，因为可能其他订阅对象也会使用该消息。
+   * 
+   * 使用示例如下:
+
+void callback(const std_msgs::Empty::ConstPtr& message)
+{
+}
+
+ros::Subscriber sub = handle.subscribe("my_topic", 1, callback);
+
+   *
+* \param M [template] M 是指消息类型
+* \param topic 订阅的话题
+* \param queue_size 消息队列长度，超出长度时，头部的消息将被弃用
+* \param fp 当订阅到一条消息时，需要执行的回调函数
+* \return 调用成功时，返回一个订阅者对象，失败时，返回空对象
+* 
+
+void callback(const std_msgs::Empty::ConstPtr& message){...}
+ros::NodeHandle nodeHandle;
+ros::Subscriber sub = nodeHandle.subscribe("my_topic", 1, callback);
+if (sub) // Enter if subscriber is valid
+{
+...
+}
+
+*/
+template<class M>
+Subscriber subscribe(const std::string& topic, uint32_t queue_size, void(*fp)(const boost::shared_ptr<M const>&), const TransportHints& transport_hints = TransportHints())
+/**
+* \brief 生成服务端对象
+*
+* 该函数可以连接到 ROS master，并提供一个具有给定名称的服务对象。
+*
+* 使用示例如下:
+\verbatim
+bool callback(std_srvs::Empty& request, std_srvs::Empty& response)
+{
+return true;
+}
+
+ros::ServiceServer service = handle.advertiseService("my_service", callback);
+\endverbatim
+*
+* \param service 服务的主题名称
+* \param srv_func 接收到请求时，需要处理请求的回调函数
+* \return 请求成功时返回服务对象，否则返回空对象:
+\verbatim
+bool Foo::callback(std_srvs::Empty& request, std_srvs::Empty& response)
+{
+return true;
+}
+ros::NodeHandle nodeHandle;
+Foo foo_object;
+ros::ServiceServer service = nodeHandle.advertiseService("my_service", callback);
+if (service) // Enter if advertised service is valid
+{
+...
+}
+\endverbatim
+
+*/
+
+/** 
+  * @brief 创建一个服务客户端对象
+  *
+  * 当清除最后一个连接的引用句柄时，连接将被关闭。
+  *
+  * @param service_name 服务主题名称
+  */
+//用法
+ /*template<class Service>
+ ServiceClient serviceClient(const std::string& service_name, bool persistent = false, 
+   8/                          const M_string& header_values = M_string())
+
+//  等待服务函数1
+/**
+ * ros::service::waitForService("addInts");
+ * \brief 等待服务可用，否则一致处于阻塞状态
+ * \param service_name 被"等待"的服务的话题名称
+ * \param timeout 等待最大时常，默认为 -1，可以永久等待直至节点关闭
+ * \return 成功返回 true，否则返回 false。
+ */
+
+
+//等待服务函数2
+/**
+* client.waitForExistence();
+* \brief 等待服务可用，否则一致处于阻塞状态
+* \param timeout 等待最大时常，默认为 -1，可以永久等待直至节点关闭
+* \return 成功返回 true，否则返回 false。
+*/
+//bool waitForExistence(ros::Duration timeout = ros::Duration(-1));
